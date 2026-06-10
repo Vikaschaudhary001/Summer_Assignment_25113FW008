4d492f4566244c611e135672c9b93c19b4361dbb
Vikaschaudhary001 <caman1778@gmail.com>
2026-06-10 22:10:24 +0530

    Add files via upload

---
+++ day_10_assignment/p38.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    for(int i=0;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+
+        }
+        for(int j=1;j<=(10-2*i-1);j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=5;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
